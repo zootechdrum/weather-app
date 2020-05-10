9e58100276fee335b8e5c126780544fb89c6bc8b
Cesar Gomez <zootechdrum@gmail.com>
2020-05-09 23:04:51 -0700

    added ajax call ensure I get data from open weather api

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,22 @@ $("#search-btn").click(()=> {
     const str = $("#search-input").val().trim()
     //Create Div element
     const div = $("<div>")
-
     const city = div.append(str)
-
+    //append city to the aside div in grid
     $(".past-city").append(city)
+
+    getForecast(str)
+
 })
 
+const getForecast = (cityName) => {
+
+    const URL = "https://api.openweathermap.org/data/2.5/forecast?q=riverside&&appid=eb24ebd17a4375e8ec365a3eba5592a2"
+
+    $.ajax({url: URL, success: function(result){
+        console.log(result)
+      }});
+}
 
 });
 
