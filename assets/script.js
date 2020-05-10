d0629b376afb1eadc88c2b14d12e535dd8fb35f8
Cesar Gomez <zootechdrum@gmail.com>
2020-05-09 21:21:17 -0700

    appends city to div

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+$(document).ready(() => {
+//GET THE VALUE OF INPUT BOX
+ 
+$("#search-btn").click(()=> {
+    const str = $("#search-input").val().trim()
+    //Create Div element
+    const div = $("<div>")
+
+    const city = div.append(str)
+
+    $(".past-city").append(city)
+})
+
+
+});
+
