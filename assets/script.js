581f6fb292e0a51218636a7b08204e6f248f68a6
Cesar Gomez <zootechdrum@gmail.com>
2020-05-10 21:50:27 -0700

    can now display city info

--- assets/script.js
+++ assets/script.js
@@ -5,16 +5,33 @@ $(document).ready(function () {
         //Use Regex to replace white space in string
         str.replace(/\s/g, "");
         //Create Div element
-        var div = $("<div>");
+        var div = $("<div class='city-name'>");
         var city = div.append(str);
         //append city to the aside div in grid
         $(".past-city").append(city);
-        getForecast(str);
+        currentWeather(str);
     });
-    var getForecast = function (cityName) {
-        var URL = "https://api.openweathermap.org/data/2.5/forecast?q=riverside&&appid=eb24ebd17a4375e8ec365a3eba5592a2";
+    // const getCurrentWeather = (cityName: string) => {
+    //     const URL = "https:api.openweathermap.org/data/2.5/weather?q=riverside&appid=eb24ebd17a4375e8ec365a3eba5592a2";
+    //     $.ajax({url: URL, success: function(result){
+    //         console.log(result)
+    //       }});
+    // };
+    var currentWeather = function (cityName) {
+        var URL = "https:api.openweathermap.org/data/2.5/weather?q=" + cityName + "&&appid=eb24ebd17a4375e8ec365a3eba5592a2";
         $.ajax({ url: URL, success: function (result) {
+                //items that will be in current-weather div
                 console.log(result);
+                var weatherItems = {
+                    city: cityName,
+                    humidity: result.main.humidity,
+                    windSpeed: result.wind.speed
+                };
+                displayWeather(weatherItems);
             } });
+    };
+    var displayWeather = function (items) {
+        var h2 = $("<h2>").append(items.city);
+        $("#current-weather").html(h2);
     };
 });
--- assets/script.ts
+++ assets/script.ts
@@ -1,33 +1,57 @@
 $(document).ready(() => {
 //GET THE VALUE OF INPUT BOX
  
-$("#search-btn").click(()=> {
+$("#search-btn").click(() => {
 
     const str = $("#search-input").val() as string
     //Use Regex to replace white space in string
     str.replace(/\s/g, "")
 
     //Create Div element
-    const div = $("<div>")
+    const div = $("<div class='city-name'>")
     const city = div.append(str)
     //append city to the aside div in grid
     $(".past-city").append(city)
 
-    getForecast(str)
+    currentWeather(str);
 
 })
 
-const getCurrentWeather = (cityName: string) => {
+// const getCurrentWeather = (cityName: string) => {
 
-};
+//     const URL = "https:api.openweathermap.org/data/2.5/weather?q=riverside&appid=eb24ebd17a4375e8ec365a3eba5592a2";
 
-const getForecast = (cityName: string) => {
+//     $.ajax({url: URL, success: function(result){
+//         console.log(result)
+//       }});
 
-    const URL = "https://api.openweathermap.org/data/2.5/forecast?q=riverside&&appid=eb24ebd17a4375e8ec365a3eba5592a2"
+// };
+
+const currentWeather = (cityName: string) => {
+
+    const URL = `https:api.openweathermap.org/data/2.5/weather?q=${cityName}&&appid=eb24ebd17a4375e8ec365a3eba5592a2`
 
     $.ajax({url: URL, success: function(result){
+        //items that will be in current-weather div
         console.log(result)
+
+        const weatherItems = {
+            city: cityName,
+            humidity: result.main.humidity,
+            windSpeed: result.wind.speed
+        }
+
+        displayWeather(weatherItems)
+
       }});
+}
+
+const displayWeather = ( items: {city:string, humidity: number, windSpeed: number}) => {
+    
+    const h2 = $("<h2>").append(items.city)
+     $("#current-weather").html(h2)
+    
+
 }
 
 });
